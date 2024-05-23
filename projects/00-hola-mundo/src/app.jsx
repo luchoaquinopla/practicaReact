5cc644fd154321cbaf4ebcbf3fa80f0893e6000a
luchoaquinopla <luchoaquinopla@gmail.com>
2024-05-23 11:15:45 -0300

    primer commit

---
+++ projects/00-hola-mundo/src/app.jsx
@@ -0,0 +1,17 @@
+export function App(){
+    return(
+       <article style ={{display:'flex',alingItems: 'center',color: '#fff'}}>
+        <header>
+            <img alt ="kikobeats" src ="https://unavatar.io/kikobeats?ttl=1h" />
+            <div>
+                <strong>Sudanalytics  </strong>
+                <span>@Sudanalytics</span>
+            </div>
+          </header>
+          <aside>
+          <button>Follow</button>
+          </aside>
+       </article>
+    )
+}
+
